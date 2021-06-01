1d1ca6c1c8afc4ee319a73d7870cd8c91430f6b1
P, Jaimi <jaimi.patel19@imperial.ac.uk>
2021-06-01 14:41:34 +0100

    feat(server): Add backend server

---
+++ backend/src/server.js
@@ -0,0 +1,34 @@
+const { Pool } = require('pg')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const router = express.Router();
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+})
+
+app.use(cors());
+app.use(express.json());
+
+router.get('/test', (req, res) => {
+  res.json({"test": true})
+})
+
+router.get('/dbCon', (req, res) => {
+  pool
+    .connect()
+    .then(() => res.json({"result": "Database connected"}))
+    .catch(err => console.log(err))
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
