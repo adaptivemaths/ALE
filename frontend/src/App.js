c4b0c0bf1ebdc267b62d18a40dc6e8f54d09082b
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-06-13 17:26:44 +0100

    Create class App for app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { Helmet } from "react-helmet";
+import HomePage from "./home_page/homePage";
+import React from 'react';
 
-function App() {
-  return (
-    <>
-    <Helmet>
-      <title>EdiCat</title>
-    </Helmet>
-      <title>Edicat</title>
-      <h1>EdiCat Learning</h1>
-    </>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+
+        <Helmet>
+          <title>EdiCat</title>
+        </Helmet>
+
+        <HomePage />
+
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
