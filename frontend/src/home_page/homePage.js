89d281febe1cce01e006947b1a9681ba08af82c9
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-06-13 17:25:34 +0100

    Create HomePage class for temporary default home page

---
+++ frontend/src/home_page/homePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./homePage.css";
+
+export default class HomePage extends React.Component {
+
+    render() {
+        return (
+            <div className="homepage">
+                <h1>EdiCat Learning</h1>
+                <p>Work in progress...</p>
+                <img src="https://png.pngitem.com/pimgs/s/207-2073499_translate-platform-from-english-to-spanish-work-in.png"></img>
+            </div>
+        )
+    }
+}
