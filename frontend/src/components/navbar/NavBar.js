a3c882413119803393b4b2374d8d772ee731d7c8
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-06-30 16:44:44 +0100

    Correct path for links

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -8,11 +8,11 @@ export default class NavBar extends React.Component {
     render() {
         return (
             <Navbar bg="light" variant="light" sticky="top">
-                <Navbar.Brand href="#home" className="brand">Adaptive Learning</Navbar.Brand>
+                <Navbar.Brand href="/" className="brand">Adaptive Learning</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#login" className="btn">Log In</Nav.Link>
-                    <Nav.Link href="#" className="btn">Try it for free</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link href="/login" className="btn">Log In</Nav.Link>
+                    <Nav.Link href="/trial" className="btn">Try it for free</Nav.Link>
                 </Nav>
             </Navbar>
         )
