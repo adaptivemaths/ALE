7c532a1c22bad9ee6311f2b352da801978fd8011
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-06-30 12:56:08 +0100

    Add NavBar component

---
+++ frontend/src/components/navbar/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import { Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./navbar.css";
+
+export default class NavBar extends React.Component {
+    render() {
+        return (
+            <Navbar bg="light" variant="light" sticky="top">
+                <Navbar.Brand href="#home" className="brand">EdiCat Learning</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#login" className="btn">Log In</Nav.Link>
+                    <Nav.Link href="#" className="btn">Try it for free</Nav.Link>
+                </Nav>
+            </Navbar>
+        )
+    }
+}
