1b9b6a6ee59c9d05866cad9fb91bed723fb4b55e
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-06-30 15:31:58 +0100

    Add information, footer and image to homepage

--- frontend/src/components/home_page/homePage.js
+++ frontend/src/components/home_page/homePage.js
@@ -1,14 +1,35 @@
 import React from "react";
 import "./homePage.css";
 import NavBar from "../navbar/NavBar";
+import laptop from "./laptop.svg";
 
 export default class HomePage extends React.Component {
 
     render() {
         return (
-            <>
+            <div className="homepage">
                 <NavBar />
-            </>
+
+                <div className="wrapper">
+                    <div className="info">
+                        <div className="big-info">
+                            We personalise the way students learn Maths
+                        </div>
+                        <div className="small-info">
+                            Our assessment platform learns your ability, creates questions that target your growth areas
+                            and provides personalised learning recommendations
+                        </div>
+                    </div>
+
+                    <div className="laptop">
+                        <img src={laptop}></img>
+                    </div>
+                </div>
+
+                <footer className="footer">
+                    We'd love to talk to you, reach out at hello@adaptivemaths.co.uk
+                </footer>
+            </div>
         )
     }
 }
