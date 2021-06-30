0e313b2de7c46108cad295fe92d54edf01c57423
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-06-30 12:56:32 +0100

    Add navbar to home page

--- frontend/src/components/home_page/homePage.js
+++ frontend/src/components/home_page/homePage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./homePage.css";
+import NavBar from "../navbar/NavBar";
 
 export default class HomePage extends React.Component {
 
     render() {
         return (
-            <div className="homepage">
-                <h1>EdiCat Learning</h1>
-                <img src="https://png.pngitem.com/pimgs/s/207-2073499_translate-platform-from-english-to-spanish-work-in.png"></img>
-            </div>
+            <>
+                <NavBar />
+            </>
         )
     }
 }
