f7ec36ecae619f26795d14216e0001a5e295dbb1
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-04 08:27:06 +0100

    Remove unnecessary code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,21 @@
+import Database from "./database";
+import { config } from "./constants";
+
 const { Pool } = require('pg')
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-})
+
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/test', (req, res) => {
   res.json({"test": true})
-})
-
-app.get('/dbCon', (req, res) => {
-  pool
-    .connect()
-    .then(() => res.json({"result": "Database connected"}))
-    .catch(err => console.log(err))
 })
 
 app.listen(port, () => {
