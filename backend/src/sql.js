c2b1cc7a38f33386f3acd5de0a3bf03d19f512f8
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-04 08:24:45 +0100

    Create QueryFile from SQL file for mailing list

--- backend/src/sql.js
+++ backend/src/sql.js
@@ -5,3 +5,8 @@ function sql(filename) {
   const sqlPath = path.join(__dirname, filename);
   return new QueryFile(sqlPath, { minify: true });
 }
+
+
+export const mailingListSQL = {
+  addMail: sql("../sql/mailing_list/addMail.sql")
+}
