4276fa2e10b820ea0d5a6f9a36d53f4249e1e647
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-04 09:18:15 +0100

    Function for POST request to add email to list

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,18 @@
 import axios from "axios";
+import { config } from "./constants";
 
-
+async function addToMailingList(data) {
+    var result = false;
+    await axios
+      .post(`${config.API_URL}/addToMailingList`, data)
+      .then((res) => {
+        const email = res.data;
+        console.log(email);
+        console.log('api call')
+        result = email;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return result;
+}
