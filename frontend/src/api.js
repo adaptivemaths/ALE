793462ddba7c04b4f6e8aaa329969857f92f0026
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-05 18:36:29 +0100

    API call for sending mailing list confirmation

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,32 @@
 import axios from "axios";
 import { config } from "./constants";
 
+const API_URL = "https://adaptivemaths.herokuapp.com";
+
+// const API_URL = "http://localhost:5000";
+
 export async function addToMailingList(data) {
     var result = false;
+    console.log(API_URL);
     await axios
-      .post(`${config.API_URL}/addToMailingList`, data)
+      .post(`${API_URL}/addToMailingList`, data)
+      .then((res) => {
+        const email = res.data;
+        console.log(email);
+        console.log('api call')
+        result = email;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return result;
+}
+
+export async function sendMailListConfirmationMail(data) {
+    var result = false;
+    console.log(API_URL);
+    await axios
+      .post(`${API_URL}/sendMail`, data)
       .then((res) => {
         const email = res.data;
         console.log(email);
