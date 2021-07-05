5f6ab0e032fe725c969ef7f13840ec0b1a79998e
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-05 18:34:16 +0100

    API endpoint to send mailing list confirmation

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import { config } from "./constants";
 const { Pool } = require('pg')
 const express = require('express');
 const cors = require('cors');
+const nodemailer = require("nodemailer");
+
 const app = express();
 
 require("dotenv").config();
@@ -16,7 +18,7 @@ app.use(express.json());
 
 app.get('/test', (req, res) => {
   res.json({"test": true})
-})
+});
 
 app.post('/addToMailingList', async (req, res) => {
   try {
@@ -25,8 +27,47 @@ app.post('/addToMailingList', async (req, res) => {
   } catch (error) {
     res.body = "Error: " + error;
   }
-})
+});
+
+app.post('/mailingListConfirmationMail', async (req, res) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'outlook',
+      auth: {
+        user: process.env.MAIL, 
+        pass: process.env.MAIL_PASS
+      }
+    });
+
+    // send mail with defined transport object
+    const mailOptions = {
+      from: 'hello@adaptivemaths.co.uk', // sender address
+      to: req.body.email, // list of receivers
+      subject: "Thanks for showing interest in Adaptive Learning!", // Subject line
+      text: "Hello world?", // plain text body
+      html: `
+          <p>
+            Hi,<br/>
+            <br/>
+            We have added you to our mailing list and we will update you when we are ready to launch.<br/>
+            See you then!
+          </p>
+        `
+         // html body
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
+});
