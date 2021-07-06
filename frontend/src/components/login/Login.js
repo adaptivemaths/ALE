926789377338134358035ec737e65759ab69be71
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-06 06:51:28 +0100

    Improve styling for login and signup forms

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -6,31 +6,33 @@ import "./login.css";
 export default class Login extends React.Component {
     render() {
         return (
-            <>
+           <div>
                 <NavBar/>
+                
+                <div className="login-title">
+                    Login
+                </div>
+
                 <div className="login-wrapper">
-                    <div className="login-title">
-                        Login
-                    </div>
                     <form>
-                        <div className="login-field">
-                            <label className="login-label">Email address*</label>
-                            <input type="email" placeholder="Enter email" />
-                        </div>
+                        <span className="login-form">
+                            <div className="login-field">
+                                <label className="login-label">Username</label>
+                                <input name="email" type="email" placeholder="Enter email" />
+                            </div>
 
-                        <div className="login-field">
-                            <label className="login-label">Password*</label>
-                            <input type="password" placeholder="Password" />
-                        </div>
+                            <div className="login-field">
+                                <label className="login-label">Password</label>
+                                <input name="password" type="password" placeholder="Password" />
+                            </div>
 
-                        <div className="login-field">
-                                <button id="login-submit">
-                                    Login
-                                </button>
-                        </div>
+                            <button id="login-submit">
+                                Login
+                            </button>
+                        </span>
                     </form>
                 </div>
-            </>
+            </div>
         );
     }
 }
--- frontend/src/components/signup/SignUp.js
+++ frontend/src/components/signup/SignUp.js
@@ -12,40 +12,41 @@ export default class SignUp extends React.Component {
                     <div className="signup-title">
                         Sign Up
                     </div>
+                    <br/>
+
+                    <div className="signup-wrapper">
+                        <form className="signup-form">
+                            <div className="signup-field">
+                                <label className="signup-label">Email address*</label>
+                                <input name="email" type="email" placeholder="Your email" />
+                            </div>
+
+                            <div className="signup-field">
+                                <label className="signup-label">First Name*</label>
+                                <input name="firstName" type="text" placeholder="Your name" />
+                            </div>
+
+                            <div className="signup-field">
+                                <label className="signup-label">Last Name*</label>
+                                <input name="lastName" type="text" placeholder="Surname" />
+                            </div>
 
 
-                    <form className="signup-form">
-                        <div className="signup-field">
-                            <label className="signup-label">First Name*</label>
-                            <input type="text" placeholder="Your name" />
-                        </div>
+                            <div className="signup-field">
+                                <label className="signup-label">Password*</label>
+                                <input name="password" type="password" placeholder="Password" />
+                            </div>
 
-                        <div className="signup-field">
-                            <label className="signup-label">Last Name*</label>
-                            <input type="text" placeholder="Surname" />
-                        </div>
+                            <div className="signup-field">
+                                <label className="signup-label">Re-enter Password*</label>
+                                <input  name="reenterPassword" type="password" placeholder="Password" />
+                            </div>
 
-                        <div className="signup-field">
-                            <label className="signup-label">Email address*</label>
-                            <input type="email" placeholder="Enter email" />
-                        </div>
-
-                        <div className="signup-field">
-                            <label className="signup-label">Password*</label>
-                            <input type="password" placeholder="Password" />
-                        </div>
-
-                        <div className="signup-field">
-                            <label className="signup-label">Re-enter Password*</label>
-                            <input type="password" placeholder="Password" />
-                        </div>
-
-                        <div className="signup-field">
-                                <button id="signin-submit">
-                                    Submit
-                                </button>
-                        </div>
-                    </form>
+                            <button id="signup-submit">
+                                Submit
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </>
         );
