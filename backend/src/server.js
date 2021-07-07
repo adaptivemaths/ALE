2104b454e57682235385f7442210e45fda8ed62a
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-07 18:34:18 +0100

    Refactor code for sending mails into function
    Send confirmation mail using addToMailingList api
    Send confirmation mail after creating new account

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,48 +33,17 @@ app.get('/test', (req, res) => {
 app.post('/addToMailingList', async (req, res) => {
   try {
     const email = await Database.addToMailingList(req.body);
+    sendMail(req.body.email, "Thanks for showing interest in Adaptive Learning!", `
+      <p>
+        Hi,<br/>
+        <br/>
+        We have added you to our mailing list and we will update you when we are ready to launch.<br/>
+        See you then!
+      </p>
+    `);
     res.json(email);
   } catch (error) {
     res.body = "Error: " + error;
-  }
-});
-
-app.post('/mailingListConfirmationMail', async (req, res) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: 'outlook',
-      auth: {
-        user: process.env.MAIL, 
-        pass: process.env.MAIL_PASS
-      }
-    });
-
-    // send mail with defined transport object
-    const mailOptions = {
-      from: 'hello@adaptivemaths.co.uk', // sender address
-      to: req.body.email, // list of receivers
-      subject: "Thanks for showing interest in Adaptive Learning!", // Subject line
-      text: "Hello world?", // plain text body
-      html: `
-          <p>
-            Hi,<br/>
-            <br/>
-            We have added you to our mailing list and we will update you when we are ready to launch.<br/>
-            See you then!
-          </p>
-        `
-         // html body
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-  } catch (error) {
-    console.log("Error: " + error);
   }
 });
 
@@ -82,6 +51,17 @@ app.post('/signup/addUser', async (req, res) => {
   try {
     req.body.password = await bcrypt.hash(req.body.password, 10);
     const user = await Database.addUser(req.body);
+    sendMail(req.body.email, "Welcome to Adaptive Learning!", `
+      <p>
+        Hi ${req.body.firstName},<br/>
+        <br/>
+        Thanks for joining Adaptive Learning. 
+        We hope you will have a great experience learning Maths on our website!
+        <br/><br/>
+        Best of luck,<br/>
+        EdiCat Team
+      </p>
+    `);
     res.json(user);
   } catch (error) {
     res.body = "Error: " + error;
@@ -111,3 +91,35 @@ app.post('/users/login', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+
+function sendMail(recepient, subject, html) {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'outlook',
+      auth: {
+        user: process.env.MAIL, 
+        pass: process.env.MAIL_PASS
+      }
+    });
+
+    // send mail with defined transport object
+    const mailOptions = {
+      from: process.env.MAIL, // sender address
+      to: recepient, // list of receivers
+      subject: subject, // Subject line
+      html: html
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+}
