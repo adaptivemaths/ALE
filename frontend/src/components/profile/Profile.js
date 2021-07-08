e256aceb28d45c73d09aa0d0dfb6f8a73507e8d0
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-08 06:31:44 +0100

    Create component for Profile page

---
+++ frontend/src/components/profile/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Profile extends React.Component {
+    render() {
+        return (
+            <div>
+                This is your profile page
+            </div>
+        );
+    }
+}
