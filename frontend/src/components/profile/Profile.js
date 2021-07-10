fa1c4eb7f98602d81be105c40ca3eb16611d23ed
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-10 12:12:34 +0100

    Add sign out button and improve info format

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -2,6 +2,8 @@ import React from "react";
 import { getUserDetails } from "../../api";
 import { instanceOf } from 'prop-types';
 import { withCookies, Cookies } from 'react-cookie';
+import NavBar from "../navbar/NavBar";
+import { Redirect } from "react-router-dom";
 
 class Profile extends React.Component {
 
@@ -12,29 +14,68 @@ class Profile extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            userDetails: ""
+            userDetails: "",
+            signedIn: true,
+            loading: true
         }
+
+        this.signOutUser = this.signOutUser.bind(this);
+
     }
 
     async componentDidMount() {
         const currentUser = this.props.cookies.get("username");
-        console.log("username=", currentUser);
         const details = await 
         getUserDetails({
             username: currentUser
         })
         .then(res => {
             this.setState({
-                userDetails: JSON.stringify(res)
+                userDetails: res,
+                loading: false
             })
         });
 
     }
 
+    signOutUser(event) {
+        this.props.cookies.remove("username");
+        this.setState({
+            userDetails: "Your are signed out",
+            signedIn: false
+        })
+    }
+
     render() {
+        if (!this.state.signedIn) {
+            return <Redirect push to="/"/>;
+        }
+        
         return (
             <div>
-                {this.state.userDetails}
+                <NavBar/>
+                {this.state.loading ?
+                    <>
+                        Loading...
+                    </> 
+                    :  
+                    <>
+                        <label>
+                            Email: {this.state.userDetails.username}
+                        </label><br/>
+                        <label>
+                            First name: {this.state.userDetails.first_name}
+                        </label><br/>
+                        <label>
+                            Last name: {this.state.userDetails.last_name}
+                        </label><br/>
+                        {this.state.signedIn ? 
+                            <button onClick={this.signOutUser}>
+                                Sign Out
+                            </button> : ""
+                        }
+                    </>
+                }
             </div>
         );
     }
