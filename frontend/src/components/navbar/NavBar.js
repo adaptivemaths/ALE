179dfd78898ff2ea930eaa1b9b6e0d5f85af9d15
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-10 12:10:56 +0100

    Display Profile button when user has signed in

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -3,19 +3,38 @@ import Navbar from "react-bootstrap/Navbar";
 import { Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "./navbar.css";
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';
 
-export default class NavBar extends React.Component {
+class NavBar extends React.Component {
+
+    static propTypes = {
+        cookies: instanceOf(Cookies).isRequired
+    };
+
+
     render() {
         return (
             <Navbar bg="light" variant="light" sticky="top">
                 <Navbar.Brand href="/" className="brand">Adaptive Learning</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="/about">About</Nav.Link>
-                    <Nav.Link href="/practice">Practice</Nav.Link>
-                    <Nav.Link href="/login" className="hollow-btn">Log In</Nav.Link>
-                    <Nav.Link href="/signup" className="filled-btn">Sign Up</Nav.Link>
+
+                    {this.props.cookies.get("username") ? 
+                        <>
+                            <Nav.Link href="/profile">Profile</Nav.Link> 
+                            <Nav.Link href="/practice">Practice</Nav.Link>
+                        </>
+                        : 
+                        <>
+                            <Nav.Link href="/login" className="hollow-btn">Log In</Nav.Link>
+                            <Nav.Link href="/signup" className="filled-btn">Sign Up</Nav.Link>
+                        </>
+                        }
                 </Nav>
             </Navbar>
         )
     }
 }
+
+export default withCookies(NavBar);
