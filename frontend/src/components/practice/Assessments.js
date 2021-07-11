3fe73ac51e710f9bb9fcc31e7ad4171cc52aee0d
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-11 18:40:31 +0100

    Define Assessments component

---
+++ frontend/src/components/practice/Assessments.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../navbar/NavBar";
+
+
+class Assessments extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+} 
+
+export default Assessments;
