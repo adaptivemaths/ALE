42fb3a73dc426ccfb405b2e44758376b6f29a81a
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-12 19:39:03 +0100

    Display names of test papers on assessments page

--- frontend/src/components/practice/Assessments.js
+++ frontend/src/components/practice/Assessments.js
@@ -1,12 +1,32 @@
 import React from "react";
 import NavBar from "../navbar/NavBar";
 
+import { getPaperNames } from "../../api";
 
 class Assessments extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            paperNames: []
+        }
+    }
+
+    async componentDidMount() {
+        const paperNames = await getPaperNames();
+        this.setState({
+            paperNames: paperNames
+        });
+    }
+
     render() {
         return (
             <div>
-                <NavBar/>
+                <NavBar/><br/>
+                {this.state.paperNames.map(paper => (
+                    <div>
+                        {paper.GCSE_Paper_Name}
+                    </div>
+                ))}
             </div>
         );
     }
