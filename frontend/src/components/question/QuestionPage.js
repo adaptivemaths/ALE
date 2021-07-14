e02f8445efd3619819699fe4f87a1e9fde11cc58
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-14 07:58:35 +0100

    Display information about question information
    Add Next and Back button to change current question

--- frontend/src/components/question/QuestionPage.js
+++ frontend/src/components/question/QuestionPage.js
@@ -8,8 +8,12 @@ export default class QuestionPage extends React.Component {
         this.state = {
             paperName: "",
             questions: [],
-            currentQuestion: 0
+            currentQuestion: 0,
+            questionsLoaded: false
         }
+
+        this.nextQuestion = this.nextQuestion.bind(this);
+        this.previousQuestion = this.previousQuestion.bind(this);
     }
     
     async componentDidMount() {
@@ -17,15 +21,99 @@ export default class QuestionPage extends React.Component {
             const questions = await getQuestions({
                 GCSE_Paper_Name: this.state.paperName
             });
-            this.setState({questions})
+            this.setState({
+                questions,
+                questionsLoaded: true
+            })
+        });
+    }
+
+    getCurrentQuestion() {
+        return this.state.questions[this.state.currentQuestion];
+    }
+
+    nextQuestion(event) {
+        event.preventDefault();
+        this.setState({
+            currentQuestion: (this.state.currentQuestion + 1) % this.state.questions.length
+        });
+    }
+
+    previousQuestion(event) {
+        event.preventDefault();
+        this.setState({
+            currentQuestion: (this.state.currentQuestion - 1) % this.state.questions.length
         });
     }
 
     render() {
         return (
-            <div>
-                {JSON.stringify(this.state.questions[this.state.currentQuestion])}
-            </div>
+        this.state.questionsLoaded ? (
+                <div>
+                    <button onClick={this.previousQuestion}>
+                        <span>Back</span> 
+                    </button>
+
+                    <button onClick={this.nextQuestion}>
+                        <span>Skip</span> 
+                    </button>
+
+                    <button onClick={this.nextQuestion}>
+                        <span>Next</span> 
+                    </button>
+
+                    <button>
+                        <span>Submit</span>
+                    </button>
+                    <div>
+                        <div>
+                            {this.getCurrentQuestion().QUESTION_NUMBER + '.' + 
+                            (this.getCurrentQuestion().SUB_QUESTION_NO ? this.getCurrentQuestion().SUB_QUESTION_NO : "")}
+                        </div>
+
+                        <div>
+                            {this.getCurrentQuestion().QUESTION_TEXT}
+                        </div>
+
+                        <div> 
+                            {this.getCurrentQuestion().QUESTION_INSTRUCTIONS}
+                        </div>
+
+                        <div>
+                            Marks: {this.getCurrentQuestion().QUESTION_MARKS}
+                        </div>
+
+                        <div>
+                            Difficulty: {this.getCurrentQuestion().GRD_DIFFICULTY}
+                        </div>
+
+                        <div>
+                            Topic: {this.getCurrentQuestion().TOPIC}
+                        </div>
+                    </div>
+
+                    <div>
+                            <div>
+                                {this.getCurrentQuestion().QUESTION_OPTION_1}
+                            </div>   
+                                        
+                            <div>
+                                {this.getCurrentQuestion().QUESTION_OPTION_2}
+                            </div>
+
+                            <div>
+                                {this.getCurrentQuestion().QUESTION_OPTION_3}
+                            </div>
+
+                            <div>
+                                {this.getCurrentQuestion().QUESTION_OPTION_4}
+                            </div>
+
+
+                    </div> 
+                </div>
+            )
+            : ""
         );
     }
 }
