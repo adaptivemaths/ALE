43b49f38c7de51ed7022310ad347c509e60b7a3b
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-16 08:36:49 +0100

    Add route to results page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ import Login from "./components/login/Login";
 import Profile from "./components/profile/Profile";
 import Assessments from './components/practice/Assessments';
 import QuestionPage from './components/question/QuestionPage';
-
+import ResultsPage from './components/results/ResultsPage';
 
 
 class App extends React.Component {
@@ -80,6 +80,8 @@ class App extends React.Component {
           </Route>
 
           <Route exact path="/practice/assessments/:paperName/" component={QuestionPage}/>
+
+          <Route exact path="/results" component={ResultsPage}/>
         </Switch>
 
       </>
