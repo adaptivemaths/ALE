492f5c633a8a510faaea4e22bc7e2f92ae05a495
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-08-19 14:34:03 +0100

    Display skills associated with given topic

--- frontend/src/components/practice/Topic.js
+++ frontend/src/components/practice/Topic.js
@@ -1,20 +1,38 @@
 import React from "react";
 import NavBar from "../navbar/NavBar";
 import "./topic.css";
+import { getSkillsForTopic } from "../../api";
 
 export default class Topic extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            learningObjective: this.props.learningObjective,
-            question: {
-
-            },
+            topic: "",
+            skills: [],
         }
     }
     
-    componentDidMount() {
-        
+    async componentDidMount() {
+        this.setState({
+            topic: this.props.match.params.topic,
+        }, async () => {
+            console.log('topic', this.state.topic);
+            this.setState({
+                skills: await getSkillsForTopic(this.state.topic),
+            });
+        });
+    }
+
+    skillsList() {
+        return (
+            <>
+                {this.state.skills.map((skill) => (
+                    <>
+                        {skill.skill_name}<br/>
+                    </>
+                ))}
+            </>
+        )
     }
 
     render() {
@@ -23,6 +41,10 @@ export default class Topic extends React.Component {
                 <NavBar/>
                 <div className="topic-title">
                     {this.state.topic}
+                </div>
+                <br/>
+                <div>
+                    {this.skillsList()}
                 </div>
             </div>
         );
