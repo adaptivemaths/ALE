66d79a89afef1e24e3addf40390a530fee588093
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-08-20 17:12:40 +0100

    Display template question
    Add buttons to check answer & generate new question

--- frontend/src/components/skill_page/SkillQuestion.js
+++ frontend/src/components/skill_page/SkillQuestion.js
@@ -6,6 +6,7 @@ import parse from "html-react-parser";
 import { getSkill } from "../../api";
 import templateQuestions from "./template_questions/templateQuestions";
 import NavBar from "../navbar/NavBar";
+import "./SkillQuestion.css";
 
 class SkillQuestion extends React.Component {
 
@@ -18,38 +19,94 @@ class SkillQuestion extends React.Component {
         this.state = {
             skillId: 0,
             skillName: '',
-            question: {},
+            question: {
+                text: '',
+                values: [],
+                checkAnswer: () => false,
+            },
             answer: {},
             newQuestion: () => {},
+            showResult: false,
+            correct: false,
         }
+
+        this.setNewQuestion = this.setNewQuestion.bind(this);
+        this.setAnswer = this.setAnswer.bind(this);
+        this.checkAnswer = this.checkAnswer.bind(this);
     }
 
     async componentDidMount() {
         const skillId = this.props.match.params.skillId;
+        const skill = await getSkill(skillId);
+        const skillName = skill.skill_name;
+        const newQuestion = templateQuestions[skillName];
+        const question = newQuestion();
 
         this.setState({
             skillId,
-        }, async () => {
-            const skill = await getSkill(this.state.skillId);
-            const skillName = skill.skill_name;
-            const newQuestion = templateQuestions[skill.skill_name];
-            const question = newQuestion();
-            this.setState({
-                skillName,
-                newQuestion,
-                question,
-            });
+            skillName,
+            newQuestion,
+            question,
         });
 
+    }
+
+    setAnswer(event) {
+        this.state.answer[event.target.name] = event.target.value;
+    }
+
+    displayQuestion() {
+        
+        return (
+            <>
+                {parse(this.state.question.text)}<br/>
+                {this.state.question.values.map((value) => (
+                    <>  
+                        <label>
+                            {value}=
+                            <input 
+                                name={value} 
+                                type='text' 
+                                value={this.state.answer[value]}
+                                onChange={this.setAnswer}
+                            >
+                            </input>
+                        </label>
+                        <br/>
+                    </>
+                ))}
+            </>
+        )
+    }
+
+    setNewQuestion() {
+        const question = this.state.newQuestion();
+        this.setState({
+            showResult: false,
+            question,
+            answer: {},
+        });
+    }
+
+    checkAnswer() {
+        this.setState({
+            showResult: true,
+            correct: this.state.question.checkAnswer(this.state.answer),
+        })
     }
 
     render() {
         return (
             <>
                 <NavBar/>
-                <div>
+                <div className="skill-container">
                     <h1>{this.state.skillName}</h1>
-                    {JSON.stringify(this.state.question)}
+                    {this.displayQuestion()}
+                    {this.state.showResult ? 'Your answer was ' + (this.state.correct ? 'correct' : 'incorrect') : ''}
+                    <br/>
+                    <button onClick={this.checkAnswer}>Check answer</button>
+                    <br/>
+                    <button onClick={this.setNewQuestion}>New question</button>
                 </div>
             </>
         )
