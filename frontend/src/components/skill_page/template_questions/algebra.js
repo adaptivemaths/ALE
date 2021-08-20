db28bd327fd2d33d92f94edaa4e4fe664a356ce5
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-08-20 18:01:27 +0100

    Fix issues with creating template question
    Make sure a is non-zero

--- frontend/src/components/skill_page/template_questions/algebra.js
+++ frontend/src/components/skill_page/template_questions/algebra.js
@@ -3,8 +3,11 @@ function getRandomInt(min, max) {
 }
 
 export function factoriseQuadraticEquation() {
-    const MAX = 5
-    const a = getRandomInt(-MAX, MAX);
+    const MAX = 5;
+    var a = getRandomInt(-MAX, MAX);
+    while (a == 0) {
+        a = getRandomInt(-MAX, MAX);
+    }
     const b = getRandomInt(-MAX, MAX);
     const c = getRandomInt(-MAX, MAX);
     const d = getRandomInt(-MAX, MAX);
@@ -13,13 +16,15 @@ export function factoriseQuadraticEquation() {
     const A = a * c;
     const B = a * d + b * c;
     const C = b * d;
-
     const text = `\
         Factorise the following quadratic:<br/>\
-        ${A > 1 ? A : ''}x² + ${B > 1 ? B : ''}x + ${C}<br/>\
+        ${A}x²
+        ${B > 0 ? '+' : ''} 
+        ${Math.abs(B) > 1 ? (B + 'x') : ''} ${C > 0 ? '+' : ''} 
+        ${Math.abs(C) > 0 ? C : ''}<br/>\
         in the form (ax + b)(cx + d).<br/>\
     `
-
+    console.log(a, b, c, d);
     return {
         text,
         values: [
