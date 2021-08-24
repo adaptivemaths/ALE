81194cda07c6348c819a12afe394f7179ae7180e
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-08-24 16:33:24 +0100

    Add link to Recommendations page on nav bar

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -3,38 +3,44 @@ import Navbar from "react-bootstrap/Navbar";
 import { Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "./navbar.css";
-import { instanceOf } from 'prop-types';
-import { withCookies, Cookies } from 'react-cookie';
+import { instanceOf } from "prop-types";
+import { withCookies, Cookies } from "react-cookie";
 
 class NavBar extends React.Component {
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired,
+  };
 
-    static propTypes = {
-        cookies: instanceOf(Cookies).isRequired
-    };
+  render() {
+    return (
+      <Navbar bg="light" variant="light" sticky="top">
+        <Navbar.Brand href="/" className="brand">
+          Adaptive Maths
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/about/">About</Nav.Link>
 
-
-    render() {
-        return (
-            <Navbar bg="light" variant="light" sticky="top">
-                <Navbar.Brand href="/" className="brand">Adaptive Maths</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="/about/">About</Nav.Link>
-
-                    {this.props.cookies.get("userId") && this.props.cookies.get("userId") != "undefined" ? 
-                        <>
-                            <Nav.Link href="/profile/">Profile</Nav.Link> 
-                            <Nav.Link href="/practice/">Practice</Nav.Link>
-                        </>
-                        : 
-                        <>
-                            <Nav.Link href="/login/" className="hollow-btn">Log In</Nav.Link>
-                            <Nav.Link href="/signup/" className="filled-btn">Sign Up</Nav.Link>
-                        </>
-                        }
-                </Nav>
-            </Navbar>
-        )
-    }
+          {this.props.cookies.get("userId") &&
+          this.props.cookies.get("userId") != "undefined" ? (
+            <>
+              <Nav.Link href="/profile/">Profile</Nav.Link>
+              <Nav.Link href="/practice/">Practice</Nav.Link>
+              <Nav.Link href="/recommendations/">Recommendations</Nav.Link>
+            </>
+          ) : (
+            <>
+              <Nav.Link href="/login/" className="hollow-btn">
+                Log In
+              </Nav.Link>
+              <Nav.Link href="/signup/" className="filled-btn">
+                Sign Up
+              </Nav.Link>
+            </>
+          )}
+        </Nav>
+      </Navbar>
+    );
+  }
 }
 
 export default withCookies(NavBar);
