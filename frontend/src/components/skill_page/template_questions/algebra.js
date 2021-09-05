84efc6af4fd50badf0a99c037670937291caabc0
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-09-05 17:59:04 +0100

    Template question for simultaneous equations

--- frontend/src/components/skill_page/template_questions/algebra.js
+++ frontend/src/components/skill_page/template_questions/algebra.js
@@ -21,15 +21,15 @@ export function factoriseQuadraticEquation() {
   const B = a * d + b * c;
   const C = b * d;
   const text = `\
-        Factorise the following quadratic:<br/>\
+        Factorise the following quadratic:
         ${A == 1 ? "" : A}x²
         ${B > 0 ? "+" : B < 0 ? "-" : ""} 
         ${B != 0 ? Math.abs(B) : ""}x 
         ${C > 0 ? "+" : C < 0 ? "-" : ""} 
-        ${C != 0 ? Math.abs(C) : ""}<br/>\
-        in the form (ax + b)(cx + d).<br/>\
+        ${C != 0 ? Math.abs(C) : ""}
+        in the form (ax + b)(cx + d).
     `;
-  console.log(a, b, c, d);
+
   return {
     text,
 
@@ -38,13 +38,46 @@ export function factoriseQuadraticEquation() {
     answer: { a, b, c, d },
 
     checkAnswer: (values) => {
-      console.log(values);
       var { a, b, c, d } = values;
       a = parseInt(a);
       b = parseInt(b);
       c = parseInt(c);
       d = parseInt(d);
       return a * c == A && a * d + b * c == B && b * d == C;
+    },
+  };
+}
+
+export function simultaneousEquations() {
+  const MAX = 10;
+  const a = getRandomInt(0, MAX, true);
+  const b = getRandomInt(0, MAX, true);
+  const c = getRandomInt(0, MAX, true);
+  const d = getRandomInt(0, MAX, true);
+  const x = getRandomInt(-MAX, MAX);
+  const y = getRandomInt(-MAX, MAX);
+  // ax + by = c
+  // dx + ey = f
+  const e = a * x + b * y;
+  const f = c * x + d * y;
+  const text = `\
+        Solve the following pair of simultaneous equations:
+        ${a !== 1 ? a : ""}x + ${b !== 1 ? b : ""}y = ${e}
+        ${c !== 1 ? c : ""}x + ${d !== 1 ? d : ""}y = ${f}
+    `;
+
+  return {
+    text,
+
+    values: ["x", "y"],
+
+    answer: { x, y },
+
+    checkAnswer: (values) => {
+      var { x, y } = values;
+      x = parseInt(x);
+      y = parseInt(y);
+      return a * x + b * y === e && c * x + d * y === f;
     },
   };
 }
