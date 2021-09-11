5f7fbc66faa4f9c7b26f2a32db24e83c86a888cd
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-09-11 18:50:55 +0100

    Add some content on About page

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -3,18 +3,17 @@ import NavBar from "../navbar/NavBar";
 import "./about.css";
 
 export default class About extends React.Component {
-    render() {
-        return (
-            <>
-                <NavBar/>
-                <div className="about-title">
-                    About
-                </div>
+  render() {
+    return (
+      <>
+        <NavBar />
+        <div className="about-title">About</div>
 
-                <div className="about-container">
-                    text
-                </div>
-            </>
-        )
-    }
+        <div className="about-container">
+          You can practice by solving full assessments or by focussing on
+          specific skills by solving the template questions.
+        </div>
+      </>
+    );
+  }
 }
